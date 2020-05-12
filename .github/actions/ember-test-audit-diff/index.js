467ffa6d6f4c3c2635066ad7ce72f63c030b5e6b
Buck Doyle <buck@hashicorp.com>
2020-05-12 10:37:07 -0500

    Fix extraction of strings

--- .github/actions/ember-test-audit-diff/index.js
+++ .github/actions/ember-test-audit-diff/index.js
@@ -7,8 +7,8 @@ const auditRegex = /Total Tests: (\d+)( \(.*\))? Total Time: (.*)ms \((.*)\)/;
 // Total Tests: 64 Total Time: 13,193ms (13s 193ms)
 // Total Tests: 65 (1 failures) Total Time: NaNms (NaNms)
 
-[, baseCountString, baseMillisecondCommaTimeString] = readFileFirstLine(baseFilePath).match(auditRegex);
-[, diffCountString, diffMillisecondCommaTimeString] = readFileFirstLine(diffFilePath).match(auditRegex);
+[, baseCountString, , baseMillisecondCommaTimeString] = readFileFirstLine(baseFilePath).match(auditRegex);
+[, diffCountString, , diffMillisecondCommaTimeString] = readFileFirstLine(diffFilePath).match(auditRegex);
 
 const baseMillisecondTimeString = parseInt(baseMillisecondCommaTimeString.replace(',', ''));
 const diffMillisecondTimeString = parseInt(diffMillisecondCommaTimeString.replace(',', ''));
