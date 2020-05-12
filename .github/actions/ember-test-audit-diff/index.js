f505dcaf3c42d9fb9844b2b8a565e88be0b996ae
Buck Doyle <buck@hashicorp.com>
2020-05-12 15:03:43 -0500

    Change to use JSON report instead of parsing

--- .github/actions/ember-test-audit-diff/index.js
+++ .github/actions/ember-test-audit-diff/index.js
@@ -1,20 +1,14 @@
 const fs = require('fs');
 const [baseFilePath, diffFilePath] = process.argv.slice(2);
 
-const auditRegex = /Total Tests: (\d+)( \(.*\))? Total Time: (.*)ms \((.*)\)/;
+const baseReport = JSON.parse(fs.readFileSync(baseFilePath));
+const diffReport = JSON.parse(fs.readFileSync(diffFilePath));
 
-// Example strings:
-// Total Tests: 64 Total Time: 13,193ms (13s 193ms)
-// Total Tests: 65 (1 failures) Total Time: NaNms (NaNms)
+const baseCount = baseReport.passes + baseReport.failures + baseReport.flaky;
+const diffCount = diffReport.passes + diffReport.failures + diffReport.flaky;
 
-[, baseCountString, , baseMillisecondCommaTimeString] = readFileFirstLine(baseFilePath).match(auditRegex);
-[, diffCountString, , diffMillisecondCommaTimeString] = readFileFirstLine(diffFilePath).match(auditRegex);
-
-const baseMillisecondTimeString = parseInt(baseMillisecondCommaTimeString.replace(',', ''));
-const diffMillisecondTimeString = parseInt(diffMillisecondCommaTimeString.replace(',', ''));
-
-const countDiff = parseInt(diffCountString) - parseInt(baseCountString);
-const timeDiff = diffMillisecondTimeString - baseMillisecondTimeString;
+const countDiff = diffCount - baseCount;
+const timeDiff = diffReport.duration - baseReport.duration;
 
 console.log(`Change in number of tests run: ${countDiff}`);
 console.log(`Change in total milliseconds for tests: ${timeDiff}`);
