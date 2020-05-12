86d4c7dcabad2480a3175566051ee37d5a6c2ec2
Buck Doyle <buck@hashicorp.com>
2020-05-12 15:57:34 -0500

    Change report to have tablular data

--- .github/actions/ember-test-audit-diff/index.js
+++ .github/actions/ember-test-audit-diff/index.js
@@ -16,8 +16,12 @@ console.log(`Change in total milliseconds for tests: ${timeDiff}`);
 const output = `
 As of ${sha.slice(0, 7)}
 
-Change in number of tests run: ${countDiff}
-Change in total milliseconds for tests: ${timeDiff}
+|               | base                   | ${sha.slice(0, 7)}     | change                                       |
+|---------------|------------------------|------------------------|----------------------------------------------|
+| passes        | ${baseReport.passes}   | ${diffReport.passes}   | ${diffReport.passes - baseReport.passes}     |
+| failures      | ${baseReport.failures} | ${diffReport.failures} | ${diffReport.failures - baseReport.failures} |
+| flaky         | ${baseReport.flaky}    | ${diffReport.flaky}    | ${diffReport.flaky - baseReport.flaky}       |
+| duration (ms) | ${baseReport.duration} | ${diffReport.duration} | ${diffReport.duration - baseReport.duration} |
 `;
 
 fs.writeFileSync('audit-diff.txt', output);
