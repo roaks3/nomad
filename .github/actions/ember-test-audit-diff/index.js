d1bf9885b11e2df5649a6b41b323001018575c70
Buck Doyle <buck@hashicorp.com>
2020-05-12 10:27:37 -0500

    Add handling for string with failures
    OBVS this parsing step should be eliminated!

--- .github/actions/ember-test-audit-diff/index.js
+++ .github/actions/ember-test-audit-diff/index.js
@@ -1,7 +1,11 @@
 const fs = require('fs');
 const [baseFilePath, diffFilePath] = process.argv.slice(2);
 
-const auditRegex = /Total Tests: (\d+) Total Time: (.*)ms \((.*)\)/;
+const auditRegex = /Total Tests: (\d+)( \(.*\))? Total Time: (.*)ms \((.*)\)/;
+
+// Example strings:
+// Total Tests: 64 Total Time: 13,193ms (13s 193ms)
+// Total Tests: 65 (1 failures) Total Time: NaNms (NaNms)
 
 [, baseCountString, baseMillisecondCommaTimeString] = readFileFirstLine(baseFilePath).match(auditRegex);
 [, diffCountString, diffMillisecondCommaTimeString] = readFileFirstLine(diffFilePath).match(auditRegex);
