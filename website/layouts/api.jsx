cef0c792e7b023ad77ca4d2a99030b6e68fdb019
Ryan Oaks <roaks3@gmail.com>
2020-05-28 17:15:33 -0400

    Remove sidenav

--- website/layouts/api.jsx
+++ website/layouts/api.jsx
@@ -1,8 +1,17 @@
 import DocsPage from '@hashicorp/react-docs-page'
-import order from '../data/api-navigation.js'
-import { frontMatter as data } from '../pages/api-docs/**/*.mdx'
 import Head from 'next/head'
 import Link from 'next/link'
+
+const STUBBED_SIDENAV_DATA = [
+  {
+    layout: 'api',
+    page_title: 'Test',
+    sidebar_title: 'namespaces',
+    description: 'Test',
+    __resourcePath: 'api-docs/namespaces.mdx',
+  },
+]
+const STUBBED_ORDER = ['namespaces']
 
 function ApiLayoutWrapper(pageMeta) {
   function ApiLayout(props) {
@@ -14,14 +23,14 @@ function ApiLayoutWrapper(pageMeta) {
           is: Head,
           title: `${pageMeta.page_title} | Nomad by HashiCorp`,
           description: pageMeta.description,
-          siteName: 'Nomad by HashiCorp'
+          siteName: 'Nomad by HashiCorp',
         }}
         sidenav={{
           Link,
           category: 'api-docs',
           currentPage: props.path,
-          data,
-          order
+          data: STUBBED_SIDENAV_DATA,
+          order: STUBBED_ORDER,
         }}
         resourceURL={`https://github.com/hashicorp/nomad/blob/master/website/pages/${pageMeta.__resourcePath}`}
       />
--- website/layouts/docs.jsx
+++ website/layouts/docs.jsx
@@ -1,12 +1,21 @@
 import DocsPage from '@hashicorp/react-docs-page'
-import order from '../data/docs-navigation.js'
-import { frontMatter } from '../pages/docs/**/*.mdx'
 import { MDXProvider } from '@mdx-js/react'
 import Placement from '../components/placement-table'
 import Head from 'next/head'
 import Link from 'next/link'
 
 const DEFAULT_COMPONENTS = { Placement }
+
+const STUBBED_SIDENAV_DATA = [
+  {
+    layout: 'docs',
+    page_title: 'Test',
+    sidebar_title: 'faq',
+    description: 'Test',
+    __resourcePath: 'docs/faq.mdx',
+  },
+]
+const STUBBED_ORDER = ['faq']
 
 function DocsLayoutWrapper(pageMeta) {
   function DocsLayout(props) {
@@ -19,14 +28,14 @@ function DocsLayoutWrapper(pageMeta) {
             is: Head,
             title: `${pageMeta.page_title} | Nomad by HashiCorp`,
             description: pageMeta.description,
-            siteName: 'Nomad by HashiCorp'
+            siteName: 'Nomad by HashiCorp',
           }}
           sidenav={{
             Link,
             category: 'docs',
             currentPage: props.path,
-            data: frontMatter,
-            order
+            data: STUBBED_SIDENAV_DATA,
+            order: STUBBED_ORDER,
           }}
           resourceURL={`https://github.com/hashicorp/nomad/blob/master/website/pages/${pageMeta.__resourcePath}`}
         />
