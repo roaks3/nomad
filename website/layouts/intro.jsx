faa18849ff4df84cc02bc4e5a87672837ca1ed6f
Ryan Oaks <roaks3@gmail.com>
2020-05-28 17:15:33 -0400

    Remove sidenav

--- website/layouts/intro.jsx
+++ website/layouts/intro.jsx
@@ -1,8 +1,17 @@
 import DocsPage from '@hashicorp/react-docs-page'
-import order from '../data/intro-navigation.js'
-import { frontMatter as data } from '../pages/intro/**/*.mdx'
 import Head from 'next/head'
 import Link from 'next/link'
+
+const STUBBED_SIDENAV_DATA = [
+  {
+    layout: 'intro',
+    page_title: 'Test',
+    sidebar_title: 'use-cases',
+    description: 'Test',
+    __resourcePath: 'intro/use-cases.mdx',
+  },
+]
+const STUBBED_ORDER = ['use-cases']
 
 function IntroLayoutWrapper(pageMeta) {
   function IntroLayout(props) {
@@ -14,14 +23,14 @@ function IntroLayoutWrapper(pageMeta) {
           is: Head,
           title: `${pageMeta.page_title} | Nomad by HashiCorp`,
           description: pageMeta.description,
-          siteName: 'Nomad by HashiCorp'
+          siteName: 'Nomad by HashiCorp',
         }}
         sidenav={{
           Link,
           category: 'intro',
           currentPage: props.path,
-          data,
-          order
+          data: STUBBED_SIDENAV_DATA,
+          order: STUBBED_ORDER,
         }}
         resourceURL={`https://github.com/hashicorp/nomad/blob/master/website/pages/${pageMeta.__resourcePath}`}
       />
