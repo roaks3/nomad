1fb1953b0fca49fb89ba7aaa86c7ad0015af4d5c
Ryan Oaks <roaks3@gmail.com>
2020-05-29 14:15:45 -0400

    Replace next-mdx-enhanced with next-mdx-remote for doc page content

---
+++ website/pages/intro/[...slug].jsx
@@ -0,0 +1,95 @@
+import fs from 'fs'
+import path from 'path'
+import { promisify } from 'util'
+import matter from 'gray-matter'
+import Head from 'next/head'
+import Link from 'next/link'
+import hydrate from 'next-mdx-remote/hydrate'
+import renderToString from 'next-mdx-remote/render-to-string'
+import DocsPage from '@hashicorp/react-docs-page'
+import {
+  anchorLinks,
+  includeMarkdown,
+  paragraphCustomAlerts,
+  typography,
+} from '@hashicorp/remark-plugins'
+import { frontMatter as sidenavData } from '../../content/intro/**/*.mdx'
+import introFiles from '../../data/.tmp/intro-files'
+import order from '../../data/intro-navigation.js'
+
+sidenavData.forEach((d) => {
+  d.__resourcePath = d.__resourcePath.replace(
+    `${process.cwd().slice(1)}/content/`,
+    ''
+  )
+})
+
+export default function IntroPage({
+  renderedContent,
+  frontMatter,
+  filePath,
+  url,
+}) {
+  const hydratedContent = hydrate(renderedContent)
+  return (
+    <DocsPage
+      product="nomad"
+      head={{
+        is: Head,
+        title: `${frontMatter.page_title} | Nomad by HashiCorp`,
+        description: frontMatter.description,
+        siteName: 'Nomad by HashiCorp',
+      }}
+      sidenav={{
+        Link,
+        category: 'intro',
+        currentPage: `/${url}`,
+        data: sidenavData,
+        order,
+      }}
+      resourceURL={`https://github.com/hashicorp/nomad/blob/master/website/${filePath}`}
+    >
+      {hydratedContent}
+    </DocsPage>
+  )
+}
+
+export async function getStaticProps({ params }) {
+  const filePath = `content/intro/${params.slug.join('/')}.mdx`
+  const url = `intro/${params.slug.join('/')}`
+  const fileContent = (
+    await promisify(fs.readFile)(`${process.cwd()}/${filePath}`)
+  ).toString()
+
+  const { content, data } = matter(fileContent)
+  const renderedContent = await renderToString(content, undefined, {
+    remarkPlugins: [
+      [includeMarkdown, { resolveFrom: path.join(process.cwd(), 'partials') }],
+      anchorLinks,
+      paragraphCustomAlerts,
+      typography,
+    ],
+  })
+
+  return {
+    props: {
+      renderedContent,
+      frontMatter: data,
+      filePath,
+      url,
+    },
+  }
+}
+
+export async function getStaticPaths() {
+  const paths = introFiles.map((filePath) => ({
+    params: {
+      slug: filePath.replace(/\.mdx/, '').split('/'),
+    },
+  }))
+
+  return {
+    paths,
+    fallback: false,
+  }
+}
