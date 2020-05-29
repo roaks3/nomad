e290dc0d54104914611210ed960f600d3c2b0cff
Ryan Oaks <roaks3@gmail.com>
2020-05-29 14:15:45 -0400

    Use server-side rendering for mdx pages

---
+++ website/pages/docs/[...slug].jsx
@@ -0,0 +1,83 @@
+import fs from 'fs'
+import path from 'path'
+import { promisify } from 'util'
+import matter from 'gray-matter'
+import Head from 'next/head'
+import Link from 'next/link'
+import highlight from '@mapbox/rehype-prism'
+import { MDXProvider } from '@mdx-js/react'
+import hydrate from 'next-mdx-remote/hydrate'
+import renderToString from 'next-mdx-remote/render-to-string'
+import DocsPage from '@hashicorp/react-docs-page'
+import {
+  anchorLinks,
+  includeMarkdown,
+  paragraphCustomAlerts,
+  typography,
+} from '@hashicorp/remark-plugins'
+import Placement from '../../components/placement-table'
+import sidenavData from '../../data/.tmp/docs-frontmatter'
+import order from '../../data/docs-navigation.js'
+
+const DEFAULT_COMPONENTS = { Placement }
+
+export default function DocsDocsPage({
+  renderedContent,
+  frontMatter,
+  filePath,
+  url,
+}) {
+  const hydratedContent = hydrate(renderedContent)
+  return (
+    <MDXProvider components={DEFAULT_COMPONENTS}>
+      <DocsPage
+        product="nomad"
+        head={{
+          is: Head,
+          title: `${frontMatter.page_title} | Nomad by HashiCorp`,
+          description: frontMatter.description,
+          siteName: 'Nomad by HashiCorp',
+        }}
+        sidenav={{
+          Link,
+          category: 'docs',
+          currentPage: `/${url}`,
+          data: sidenavData,
+          order,
+        }}
+        resourceURL={`https://github.com/hashicorp/nomad/blob/master/website/${filePath}`}
+      >
+        {hydratedContent}
+      </DocsPage>
+    </MDXProvider>
+  )
+}
+
+export async function getServerSideProps({ params }) {
+  const filePath = `content/docs/${params.slug.join('/')}.mdx`
+  const url = `docs/${params.slug.join('/')}`
+
+  const fileContent = (
+    await promisify(fs.readFile)(`${process.cwd()}/${filePath}`)
+  ).toString()
+
+  const { content, data } = matter(fileContent)
+  const renderedContent = await renderToString(content, DEFAULT_COMPONENTS, {
+    remarkPlugins: [
+      [includeMarkdown, { resolveFrom: path.join(process.cwd(), 'partials') }],
+      anchorLinks,
+      paragraphCustomAlerts,
+      typography,
+    ],
+    rehypePlugins: [[highlight, { ignoreMissing: true }]],
+  })
+
+  return {
+    props: {
+      renderedContent,
+      frontMatter: data,
+      filePath,
+      url,
+    },
+  }
+}
---
+++ website/scripts/print-docs-files.js
@@ -0,0 +1,34 @@
+const fs = require('fs')
+const { promisify } = require('util')
+
+async function getAllFilesRecursively(dirPath) {
+  const files = await promisify(fs.readdir)(dirPath, {
+    withFileTypes: true,
+  })
+
+  const childFilePaths = files
+    .filter((file) => file.isFile())
+    .map((file) => `${dirPath}/${file.name}`)
+
+  const descendantFilePaths = (
+    await Promise.all(
+      files
+        .filter((file) => file.isDirectory())
+        .map((file) => getAllFilesRecursively(`${dirPath}/${file.name}`))
+    )
+  ).reduce((acc, val) => acc.concat(val), [])
+
+  return [...childFilePaths, ...descendantFilePaths]
+}
+
+function printAllFilesRecursively(dirPath) {
+  getAllFilesRecursively(dirPath).then((filePaths) => {
+    console.log('module.exports = [')
+    filePaths.forEach((filePath) =>
+      console.log(`  '${filePath.replace(new RegExp(`^${dirPath}/`), '')}',`)
+    )
+    console.log(']')
+  })
+}
+
+printAllFilesRecursively(`${process.cwd()}/content/docs`)
