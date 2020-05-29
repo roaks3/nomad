d67bb8a8ec0c535de1f49826b01205fb62603251
Ryan Oaks <roaks3@gmail.com>
2020-05-29 14:15:45 -0400

    Use server-side rendering for mdx pages

---
+++ website/pages/intro/[...slug].jsx
@@ -0,0 +1,76 @@
+import fs from 'fs'
+import path from 'path'
+import { promisify } from 'util'
+import matter from 'gray-matter'
+import Head from 'next/head'
+import Link from 'next/link'
+import highlight from '@mapbox/rehype-prism'
+import hydrate from 'next-mdx-remote/hydrate'
+import renderToString from 'next-mdx-remote/render-to-string'
+import DocsPage from '@hashicorp/react-docs-page'
+import {
+  anchorLinks,
+  includeMarkdown,
+  paragraphCustomAlerts,
+  typography,
+} from '@hashicorp/remark-plugins'
+import sidenavData from '../../data/.tmp/intro-frontmatter'
+import order from '../../data/intro-navigation.js'
+
+export default function IntroPage({
+  renderedContent,
+  frontMatter,
+  filePath,
+  url,
+}) {
+  const hydratedContent = hydrate(renderedContent)
+  return (
+    <DocsPage
+      product="nomad"
+      head={{
+        is: Head,
+        title: `${frontMatter.page_title} | Nomad by HashiCorp`,
+        description: frontMatter.description,
+        siteName: 'Nomad by HashiCorp',
+      }}
+      sidenav={{
+        Link,
+        category: 'intro',
+        currentPage: `/${url}`,
+        data: sidenavData,
+        order,
+      }}
+      resourceURL={`https://github.com/hashicorp/nomad/blob/master/website/${filePath}`}
+    >
+      {hydratedContent}
+    </DocsPage>
+  )
+}
+
+export async function getServerSideProps({ params }) {
+  const filePath = `content/intro/${params.slug.join('/')}.mdx`
+  const url = `intro/${params.slug.join('/')}`
+  const fileContent = (
+    await promisify(fs.readFile)(`${process.cwd()}/${filePath}`)
+  ).toString()
+
+  const { content, data } = matter(fileContent)
+  const renderedContent = await renderToString(content, undefined, {
+    remarkPlugins: [
+      [includeMarkdown, { resolveFrom: path.join(process.cwd(), 'partials') }],
+      anchorLinks,
+      paragraphCustomAlerts,
+      typography,
+    ],
+    rehypePlugins: [[highlight, { ignoreMissing: true }]],
+  })
+
+  return {
+    props: {
+      renderedContent,
+      frontMatter: data,
+      filePath,
+      url,
+    },
+  }
+}
